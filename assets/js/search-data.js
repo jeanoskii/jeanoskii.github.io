730e20e7c503ca200f954d5d1be7c9100b4faae6
jeanoskii <7823118+jeanoskii@users.noreply.github.com>
2026-01-06 11:04:08 +0000

    Deploying to gh-pages from @ jeanoskii/jeanoskii.github.io@c3788a32e0f6d473de0bded7a7ade5112be86458 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,7 +448,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%65%61%6E%6F.%65%72%6D%69%74%61%6E%6F@%6C%69%76%65.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/jeanoskii# your GitHub user name", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -458,24 +465,17 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/jeanoski", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-stackoverflow',
+        title: 'Stackoverflow',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://stackoverflow.com/users/2550896", "_blank");
         },
       },];
